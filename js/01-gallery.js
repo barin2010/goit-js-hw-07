5a71bc2ba6d5c65f0e901643d82492882a7927ce
barin2010 <uyrirudenko@gmail.com>
2023-10-13 18:57:25 +0300

    add images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
+import * as basicLightbox from 'basiclightbox'
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join('');
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const instance = basicLightbox.create(`
+      <img width="800" height="600" src="${event.target.dataset.source}">
+    `);
+    instance.show();
+  }
+});
