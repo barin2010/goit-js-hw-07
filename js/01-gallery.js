936a725d42fa61531810779101ca0c2ef4c846cb
barin2010 <uyrirudenko@gmail.com>
2023-10-14 20:31:04 +0300

    ready task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,11 @@
-import * as basicLightbox from 'basiclightbox'
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-gallery.innerHTML = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-      <li class="gallery__item">
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
@@ -15,17 +14,28 @@ gallery.innerHTML = galleryItems
             alt="${description}"
           />
         </a>
-      </li>
-    `;
-  })
+      </li>`
+  )
   .join('');
+
+gallery.innerHTML = galleryMarkup;
 
 gallery.addEventListener('click', (event) => {
   event.preventDefault();
+
   if (event.target.classList.contains('gallery__image')) {
     const instance = basicLightbox.create(`
       <img width="800" height="600" src="${event.target.dataset.source}">
     `);
     instance.show();
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    const instance = basicLightbox.getInstance();
+    if (instance) {
+      instance.close();
+    }
   }
 });
